9380f4f97e488961f97741dd01921fc711b3b3ac
Zach Gazak <jzgazak@gmail.com>
2016-05-24 11:35:39 +1000

    update .sh

--- new-ec2.sh
+++ new-ec2.sh
@@ -1,8 +1,6 @@
 
 if ! command -v docker >/dev/null; then
-  sudo apt-get install -y yum
-  sudo yum update -y
-  sudo yum install -y docker
+  sudo apt-get install -y docker
   sudo service docker start
   sudo usermod -a -G docker ubuntu
   echo "Log out, log in, source new-ec2.sh"
